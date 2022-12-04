c83bbd51cb64914cd13511a7afad8f30778013ef
Tiago <tiagosts99@gmail.com>
2022-12-03 23:12:54 -0300

    ArrumarView

--- src/Pages/top/Top.tsx
+++ src/Pages/top/Top.tsx
@@ -1,26 +1,35 @@
 //import React, { useState } from "react";
 import React, { useEffect, useState } from "react";
 import Footer from "../../componentes/Footer";
-import Padrao from "../../componentes/Padrao";
+import Header from "../../componentes/Header";
+//import Padrao from "../../componentes/Padrao";
 import "./Top.css";
 
 const Top = () => {
   const [mangaTrends, setMangaTrends] = useState([]);
+  const [mangaTrends2, setMangaTrends2] = useState([]);
+  const [mangaTrends3, setMangaTrends3] = useState([]);
+  
   useEffect(() => {
     fetch("https://kitsu.io/api/edge/trending/manga")
       .then((response) => response.json())
       .then((response) => setMangaTrends(response.data));
+    fetch("https://kitsu.io/api/edge/manga?page[limit]=20&page[offset]=23")
+      .then((response) => response.json())
+      .then((response) => setMangaTrends2(response.data));
+    fetch("https://kitsu.io/api/edge/manga?page[limit]=20&page[offset]=33")
+      .then((response) => response.json())
+      .then((response) => setMangaTrends3(response.data));
   });
   return (
     <div>
-      <div>
-        <Padrao />
-      </div>
-      <div className="trending">
-        <div className="trend">
-          <ul>
-            {mangaTrends
-              ? mangaTrends.map(
+      <Header />
+
+      <div className="trend">
+        <h2>Top Trend Manga</h2>
+        <ul>
+          {mangaTrends
+            ? mangaTrends.map(
                 ({
                   id,
                   attributes: {
@@ -28,11 +37,7 @@ const Top = () => {
                   },
                 }) => (
                   <li>
-                    <a
-                      href={
-                        "https://kitsu.io/api/edge/manga/" + id + "/chapters"
-                      }
-                    >
+                    <a href={"##"}>
                       <img
                         key={id}
                         src={original ?? "imagem nao encontrada"}
@@ -42,9 +47,48 @@ const Top = () => {
                   </li>
                 )
               )
-              : "Nenhuma manga encontrado"}
-          </ul>
-        </div>
+            : "Nenhuma manga encontrado"}
+          {mangaTrends2
+            ? mangaTrends2.map(
+                ({
+                  id,
+                  attributes: {
+                    posterImage: { original },
+                  },
+                }) => (
+                  <li>
+                    <a href={"##"}>
+                      <img
+                        key={id}
+                        src={original ?? "imagem nao encontrada"}
+                        alt="manga"
+                      ></img>
+                    </a>
+                  </li>
+                )
+              )
+            : "Nenhuma manga encontrado"}
+            {mangaTrends3
+            ? mangaTrends3.map(
+                ({
+                  id,
+                  attributes: {
+                    posterImage: { original },
+                  },
+                }) => (
+                  <li>
+                    <a href={"##"}>
+                      <img
+                        key={id}
+                        src={original ?? "imagem nao encontrada"}
+                        alt="manga"
+                      ></img>
+                    </a>
+                  </li>
+                )
+              )
+            : "Nenhuma manga encontrado"}
+        </ul>
       </div>
       <Footer />
     </div>
