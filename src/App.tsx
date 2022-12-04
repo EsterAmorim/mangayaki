9bb57eb88c90d503fdc7343780646c81e3735e07
EsterAmorim <eamorimsouza@outlook.com>
2022-12-04 16:01:48 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import HomePage from './Pages/home/HomePage';
 import UserPage from './Pages/users/UserPage';
 import RegisterPage from './Pages/register/RegisterPage';
 import InteressePage from './Pages/interesse/InteressePage';
-//import ViewG from './Pages/viewg/ViewG';
+import ViewG from './Pages/viewg/ViewG';
 import Recentes from './Pages/recentes/Recentes';
 import Top from './Pages/top/Top';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 
 function App() {
 
@@ -20,6 +19,7 @@ function App() {
       <Route path='/' element={<HomePage/>}/>
       <Route path='/userhome' element={<UserPage/>}/>
       <Route path='/interesse' element={<InteressePage/>} />
+      <Route path='/viewg' element={<ViewG/>}/>
       <Route path='/recentes' element={<Recentes/>}/>
       <Route path='/top' element={<Top/>}/>
     </Routes>
